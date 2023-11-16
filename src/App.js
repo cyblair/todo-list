36f26acec78d5c148045a19ffe30eb93a31cd372
cyblair <cy@theblairs.us>
2023-11-16 18:40:45 -0500

    Lab 08 completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import { useRef, useState } from "react";
 import './App.css';
 
 function App() {
+  const [task, setTask] = useState([]);
+  const inputRef = useRef(null);
+
+  const addItem = (e) => {
+    e.preventDefault();
+    const newItem = inputRef.current.value;
+    if (newItem !== "") {
+      setTask([...task, newItem]);
+    }
+    inputRef.current.value = "";
+  };
+
+  const removeItem = (index) => {
+    const updatedTasks = task.filter((_, i) => i !== index);
+    setTask(updatedTasks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Navbar /> */}
+      <div className="App">
+        <h1>To Do List</h1>
+        <input type="text" placeholder="Enter an item" ref={inputRef} />
+        <ul>
+          {task.map((task, index) => (
+            <li className="li-item" key={index}>
+              {task}
+              <button className="remove-btn" onClick={() => removeItem(index)}>
+                X
+              </button>
+            </li>
+          ))}
+        </ul>
+        <button onClick={addItem}>
+          Add
+        </button>
+      </div>
+    </>
   );
 }
 
---
+++ src/Background.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+function Background()
+{
+    return (
+        <div className='bg-image'></div>
+    );
+}
+
+export default Background;
